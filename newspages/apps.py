a429b6c4d8a0787ecb3c414f8674517b8fcff716
Pierre Kimbanzi Ruganda <kimbanzip@gmail.com>
2020-09-21 11:30:03 +0200

    initial commit

---
+++ newspages/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NewspagesConfig(AppConfig):
+    name = 'newspages'
---
+++ newspages/views.py
@@ -0,0 +1,25 @@
+import os
+from django.shortcuts import render
+from newsapi import NewsApiClient
+
+# Create your views here.
+def Test(request):
+    api_key = os.environ.get('NEWS_API_KEY')
+    newsapi = NewsApiClient(api_key)
+    headlines = newsapi.get_top_headlines(sources="bbc-news")
+
+    articles = headlines['articles']
+    description =  []
+    news = []
+    images = []
+
+    for i in range(len(articles)):
+        set_of_articles = articles[i]
+
+        description.append(set_of_articles['description'])
+        news.append(set_of_articles['title'])
+        images.append(set_of_articles['urlToImage'])
+
+    news_list = zip(news, description, images)
+
+    return render(request, 'newspages/base.html', context={"news_list":news_list})
