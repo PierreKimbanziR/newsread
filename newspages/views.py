0378ecd046463d9535f0aaaab44d1e029ac5c0e9
Pierre Kimbanzi Ruganda <kimbanzip@gmail.com>
2020-09-26 17:28:22 +0200

    add styles

--- newspages/views.py
+++ newspages/views.py
@@ -81,5 +81,30 @@ def AP(request):
     return render(request, 'newspages/news.html', context={"news_list":news_list, "journal_name":journal_name})
 
 def home(request):
-    return render(request, 'newspages/home.html')
+    api_key = os.environ.get('NEWS_API_KEY')
+    newsapi = NewsApiClient(api_key)
+    headlines = newsapi.get_top_headlines(sources='bbc-sport')
+
+
+    articles = headlines['articles']
+    description =  []
+    news = []
+    images = []
+    links_to_article = []
+    journal_name = articles[0]['source']['name']
+
+    for i in range(len(articles)):
+        set_of_articles = articles[i]
+
+        description.append(set_of_articles['description'])
+        news.append(set_of_articles['title'])
+        images.append(set_of_articles['urlToImage'])
+        links_to_article.append(set_of_articles['url'])
+    news_list = zip(news, description, images, links_to_article)
+
+
+
+
+
+    return render(request, 'newspages/home.html', context={"news_list": news_list, "journal_name": journal_name})
 
