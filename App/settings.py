686a92386177e28cfffe3c28949b7e0cd25c6c09
Pierre Kimbanzi Ruganda <kimbanzip@gmail.com>
2020-09-29 14:12:42 +0200

    add password reset email

--- App/settings.py
+++ App/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import mimetypes
-
+import  os
 
 mimetypes.add_type("text/css", ".css", True)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,4 +132,9 @@ LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = 'bbc-news'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_ADRESS')
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
